b292e82c052d7b885e662758c66a4e50cb496eaa
leotungle <leotungle@gmail.com>
2013-06-12 00:57:48 -0400

    more args and expressions

--- ns/namespace.scm
+++ ns/namespace.scm
@@ -15,20 +15,22 @@
 			    #f)))
 	       (if fn (apply fn args) 'no-function)))))))
 
-(define (declarer ns-name fn-name fn-body)
+(define (declarer ns-name fn-sig fn-body)
   (list
    (list
     'define ns-name (list 'make-the-function))
    (list 'define (list 'create)
-	 (list 'define (list fn-name) fn-body)
-	 (list ns-name (list 'quote 'add) (list 'quote fn-name) fn-name))
+	 (list 'define fn-sig fn-body)
+	 (list ns-name (list 'quote 'add) (list 'quote (car fn-sig)) (car fn-sig)))
    (list 'create)))
 
 (define-syntax ns
   (syntax-rules (define)
-    ((ns ns-name (define (fn-name) fn-body))
-     (map eval (declarer 'ns-name 'fn-name 'fn-body)))))
-		    
+    ((ns ns-name (define (fn-name ...) fn-body))
+     (map eval (declarer 'ns-name '(fn-name ...) (quote fn-body))))
+    ((ns ns-name (define (fn-name ...) fn-body ...))
+     (ns ns-name (define (fn-name ...) (begin fn-body ...))))))
+
 ; (ns fooland (define (test) 'hey))
 
 #; (ns barman
@@ -37,3 +39,11 @@
 	(cond ((< x 10)
 	       (begin (pp x) (loop (+ x 1))))
 	      (else 'done)))))
+
+#; (ns donkey
+      (define (mule a b)
+	(define (pow a)
+	  (let loop ((i 0) (x 1))
+	    (cond ((< i a) (loop (+ i 1) (* x 2)))
+		  (else x))))
+	(+ (pow a) (pow b))))
